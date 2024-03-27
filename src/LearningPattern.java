e86ca58c9716fd7c46cd49f7190dbff257740880
BikiLearner <97147323+BikiLearner@users.noreply.github.com>
2024-03-27 19:54:15 +0530

    Pattern Commit

--- src/LearningPattern.java
+++ src/LearningPattern.java
@@ -164,5 +164,87 @@ public class LearningPattern {
             }
         }
     }
+    public static void pattern19(int n){
+        for(int i=n-1;i>=0;i--){
+            for (int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            for (int j=2;j<(n-i)*2;j++){
+                System.out.print(" ");
+            }
+            for (int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i=0;i<n;i++){
+            for (int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            for (int j=2;j<(n-i)*2;j++){
+                System.out.print(" ");
+            }
+            for (int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+    public static void pattern20(int n){
+
+        for(int i=0;i<n;i++){
+            for (int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            for (int j=2;j<(n-i)*2;j++){
+                System.out.print(" ");
+            }
+            for (int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=n-2;i>=0;i--){
+            for (int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            for (int j=2;j<(n-i)*2;j++){
+                System.out.print(" ");
+            }
+            for (int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+    public static void pattern21(int n){
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 || j==0 || i==n-1 || j==n-1){
+                    System.out.print("*");
+                }else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+    public static void pattern22(int n){
+
+        for(int i=0;i<2*n-1;i++){
+            for(int j=0;j<2*n-1;j++){
+                int right=(2*n-2)-j;
+                int down=(2*n-2)-i;
+                System.out.print(n-Math.min(Math.min(i,down),Math.min(j,right)));
+            }
+
+            System.out.println();
+        }
+
+    }
 
 }
